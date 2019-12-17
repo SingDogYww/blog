f324ba8e167e791e0a938d600fa57cdfd9169279
SingDogYww <2281435866@qq.com>
2019-12-17 22:11:23 +0800

    建立项目..

---
+++ blog/permit-service/src/main/java/com/hejiaba/permit/PermitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hejiaba.permit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermitServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermitServiceApplication.class, args);
+    }
+
+}
